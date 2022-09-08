a205bcddeb3a20c4bd940a4fae7efe78887b92d5
GrennKren <34759917+GrennKren@users.noreply.github.com>
2022-09-08 13:22:31 +0800

    Update bot.py
    Add CommandHandler

--- bot.py
+++ bot.py
@@ -7,7 +7,7 @@ from PIL import Image
 import os
 from dotenv import load_dotenv
 from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup
-from telegram.ext import ApplicationBuilder, CallbackQueryHandler, ContextTypes, MessageHandler, filters
+from telegram.ext import ApplicationBuilder, CallbackQueryHandler, ContextTypes, MessageHandler, CommandHandler, filters
 from io import BytesIO
 import random
 
