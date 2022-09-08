591d5ca6dee6658c722edceceed882a74e1abafc
GrennKren <34759917+GrennKren@users.noreply.github.com>
2022-09-08 19:03:21 +0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -168,7 +168,7 @@ async def anyGuidance_scale(update: Update, context: ContextTypes.DEFAULT_TYPE) 
 async def anyNumber(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     anyCommands('NUMBER_IMAGES')
  
-def anyCommands(options) -> None:
+async def anyCommands(options) -> None:
     if len(context.args) < 1:
         result = OPTIONS_U.get(update.message.from_user['id']).get(options)
         if result == none:
