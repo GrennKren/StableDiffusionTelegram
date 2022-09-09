773724b4e64cc52f53755876cb03f2ef1ffb5c9a
GrennKren <34759917+GrennKren@users.noreply.github.com>
2022-09-09 09:09:46 +0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -195,7 +195,7 @@ async def button(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
 
     await query.answer()
     progress_msg = await query.message.reply_text("Generating image...", reply_to_message_id=replied_message.message_id)
-
+    print(update)
     if query.data == "TRYAGAIN":
         if replied_message.photo is not None and len(replied_message.photo) > 0 and replied_message.caption is not None:
             photo_file = await replied_message.photo[-1].get_file()
@@ -213,7 +213,7 @@ async def button(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
         prompt = replied_message.text if replied_message.text is not None else replied_message.caption
         prompt = prompt if prompt[0] != "/seed" else " ".join(prompt.split(" ")[1:])
         im, seed = generate_image(prompt, photo=photo, number_images=1, user_id=update.message.from_user['id'])
-    print(update)
+    
     await context.bot.delete_message(chat_id=progress_msg.chat_id, message_id=progress_msg.message_id)
     await context.bot.send_photo(update.effective_user.id, image_to_bytes(im), caption=f'"{prompt}" (Seed: {seed[0]})', reply_markup=get_try_again_markup(), reply_to_message_id=replied_message.message_id)
 
