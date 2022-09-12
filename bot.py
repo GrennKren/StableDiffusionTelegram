1ced60ed733faa0869bcd0a66bca50ba762125f0
GrennKren <34759917+GrennKren@users.noreply.github.com>
2022-09-13 00:39:37 +0800

    update bot.py

--- bot.py
+++ bot.py
@@ -308,8 +308,8 @@ async def button(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
         if u_model_esrgan == 'face':
             _, _, output = face_enhancer.enhance(photo, has_aligned=False, only_center_face=False, paste_back=True)
         else:
-          #output, _ = upsampler.enhance(cv2.imdecode(np.asarray(photo), -1), outscale=4)
-          output, _ = upsampler.enhance(cv2.imdecode(photo, -1), outscale=4)  
+          output, _ = upsampler.enhance(cv2.imdecode(np.asarray(photo), -1), outscale=4)
+           
         
             
     await context.bot.delete_message(chat_id=progress_msg.chat_id, message_id=progress_msg.message_id)
