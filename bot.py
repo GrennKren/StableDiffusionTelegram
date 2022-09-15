3197703d95c0147588ccc9478a66970bb1c4ab2b
GrennKren <34759917+GrennKren@users.noreply.github.com>
2022-09-15 19:56:40 +0800

    update bot.py

--- bot.py
+++ bot.py
@@ -111,7 +111,7 @@ def image_to_bytes(image):
     return bio
 
 def get_try_again_markup():
-    keyboard = [[InlineKeyboardButton("Try again", callback_data={"TRYAGAIN"}), InlineKeyboardButton("Variations", callback_data="VARIATIONS")],\
+    keyboard = [[InlineKeyboardButton("Try again", callback_data={"TRYAGAIN"}), InlineKeyboardButton("Variations", callback_data={"VARIATIONS"})],\
                 [InlineKeyboardButton("Upscaling", callback_data={"UPSCALE4"})]]
     reply_markup = InlineKeyboardMarkup(keyboard)
     return reply_markup
