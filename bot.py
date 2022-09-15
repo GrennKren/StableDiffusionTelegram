ce086ba71738210fb0df03bddc528e4d341ae431
GrennKren <34759917+GrennKren@users.noreply.github.com>
2022-09-15 22:39:55 +0800

    update bot.py

--- bot.py
+++ bot.py
@@ -365,8 +365,9 @@ async def button(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
        # I know.. not too shiny. searching filename on message text.
        # I just can't figure out how to passing data into keyboardmarkup.
        # callback_data only allow string and 64Bytes lengths.
-       print(replied_message.caption)
-       filename = re.findall("[0-9]+\.?(?:png|jpeg)", replied_message.caption)[-1]
+       print("caption : " + replied_message.caption)
+       print("text : " + replied_message.text)
+       filename = re.findall("[0-9]+\.?(?:png|jpeg)", replied_message.text)[-1]
        print(type(filename))
        await context.bot.delete_message(chat_id=progress_msg.chat_id, message_id=progress_msg.message_id)
        await context.bot.send_document(update.effective_user.id, document=f'{save_location}/{filename}', reply_to_message_id=replied_message.message_id)
