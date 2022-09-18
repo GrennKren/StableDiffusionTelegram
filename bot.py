1898473fc14ebd348b6a32162d40cdc4e57e689f
GrennKren <34759917+GrennKren@users.noreply.github.com>
2022-09-18 17:57:15 +0800

    update bot.py

--- bot.py
+++ bot.py
@@ -415,8 +415,8 @@ async def button(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
             _, _, output = face_enhancer.enhance(cv2.imdecode(np.array(photo)), has_aligned=False, only_center_face=False, paste_back=True)
         else:
           print(query.message)
-          photo
-          cv2.imdecode(np.asarray(Image.open(photo).tobytes()), -1)
+          #photo
+          #cv2.imdecode(np.asarray(Image.open(photo).tobytes()), -1)
           output, _ = upsampler.enhance(cv2.imdecode(np.asarray(photo), -1), outscale=4)
            
     if query.data == "UPSCALE4":
