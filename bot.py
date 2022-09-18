292e909479984116255548c985f3d027c1e2d04b
GrennKren <34759917+GrennKren@users.noreply.github.com>
2022-09-18 17:36:54 +0800

    update bot.py

--- bot.py
+++ bot.py
@@ -413,7 +413,7 @@ async def button(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
         if u_model_esrgan == 'face':
             _, _, output = face_enhancer.enhance(cv2.imdecode(np.array(photo)), has_aligned=False, only_center_face=False, paste_back=True)
         else:
-          len(np.asarray(photo))
+          photo
           cv2.imdecode(np.asarray(Image.open(photo).tobytes()), -1)
           output, _ = upsampler.enhance(cv2.imdecode(np.asarray(photo), -1), outscale=4)
            
