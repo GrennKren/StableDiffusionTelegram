692a8450b6135b592bb748e60479f943de41462a
GrennKren <34759917+GrennKren@users.noreply.github.com>
2022-09-18 17:11:50 +0800

    update bot.py

--- bot.py
+++ bot.py
@@ -366,8 +366,7 @@ async def button(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
         photo = await photo_file.download_as_bytearray()
         
     await query.answer()
-    
-    #progress_msg = await query.message.reply_text("Generating image...", reply_to_message_id=replied_message.message_id)
+  
     progress_msg = await query.message.reply_text("Generating image...", reply_to_message_id=query.message.message_id)
     if query.data == "TRYAGAIN":
         if replied_message.photo is not None and len(replied_message.photo) > 0 and replied_message.caption is not None:
