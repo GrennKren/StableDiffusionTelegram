f644961de3df5100cbc1650849816d6120aeb463
GrennKren <34759917+GrennKren@users.noreply.github.com>
2022-09-19 18:11:00 +0800

    update bot.py

--- bot.py
+++ bot.py
@@ -75,7 +75,6 @@ if os.path.exists('/content/drive/MyDrive/Colab/StableDiffusionTelegram/' + OPTI
 
 scheduler = DDIMScheduler(beta_start=0.00085, beta_end=0.012, beta_schedule="scaled_linear", clip_sample=False, set_alpha_to_one=False) if SCHEDULER is "ddim" else \
             LMSDiscreteScheduler(beta_start=0.00085, beta_end=0.012,  beta_schedule="scaled_linear") if SCHEDULER is "klms" else \
-
             None
 
 
