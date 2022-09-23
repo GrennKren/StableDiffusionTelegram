f945b9df76d77cf73d18665d465557168e920ce5
GrennKren <34759917+GrennKren@users.noreply.github.com>
2022-09-23 10:31:20 +0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -488,11 +488,7 @@ async def button(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
           await context.bot.send_document(update.effective_user.id, document=f'{save_location}/{filename}', reply_to_message_id=replied_message.message_id)
        else:
           photo_file = await query.message.photo[-1].get_file()
-          with open(photo_file.file_path, 'rb') as file:
-            photo = BytesIO()
-            Image.frombytes(file.read()).save(photo, 'jpeg', quality=95)
-            
-            await context.bot.send_document(update.effective_user.id, document=photo.getvalue(), reply_to_message_id=replied_message.message_id)
+          await context.bot.send_document(update.effective_user.id, document=photo_file.file_path, reply_to_message_id=replied_message.message_id)
        await context.bot.delete_message(chat_id=progress_msg.chat_id, message_id=progress_msg.message_id)
     elif query.data == "INPAINT":
        context.user_data['base_inpaint'] = photo
